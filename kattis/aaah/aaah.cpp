8176b67b13f15205ad560a5f8d6497167cad9340
ongspxm <ongspxm@gmail.com>
2018-12-14 14:46:47 +0800

    aaah

---
+++ kattis/aaah/aaah.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define let const auto
+#define REP(var, i) for (int var = 0; var < i; ++var)
+using namespace std;
+using Int = int;
+//using Int = long long;
+using vi = vector<Int>;
+using vvi = vector< vector<Int> >;
+
+/**
+ * Solution for problem: aaah
+ *
+ * Written by: ZZZ
+ *
+ * Date: 2018-12-14 06:34:40.946010 UTC
+ *
+ * Lessons learned: TEXTTEXTTEXT
+ */
+void solve() {
+    string a, b;
+    cin >> a >> b;
+
+    if (a.length() >= b.length()) {
+        cout << "go" << endl;
+    } else {
+        cout << "no" << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+}
---
+++ kattis/aaah/aaah.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import collections
+import decimal
+import fractions
+import functools
+import heapq
+import itertools
+import math
+import random
+import re
+import sys
+
+### Helpers ###
+
+def readline():
+    return sys.stdin.readline().strip()
+
+def readlines():
+    return sys.stdin.readlines().strip()
+
+def read_intline():
+    return list(map(int, readline().split()))
+
+def read_floatline():
+    return list(map(float, readline().split()))
+
+memoize = functools.lru_cache
+
+
+### Implementation ###
+
+
+def main():
+    '''Solution for problem: aaah
+
+    Written by: ZZZ
+
+    Date: 2018-12-14 06:31:23.083150 UTC
+
+    Lessons learned: TEXTTEXTTEXT
+
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    main()
